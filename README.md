bc73e5065bf4cb296d736663274f4525f16bf78b
Leo Romanovsky <leoromanovsky@gmail.com>
2021-07-24 00:09:16 -0700

    add express generated app

---
+++ README.md
@@ -0,0 +1,15 @@
+# Node Express Framework with Typescript
+
+## Local Development
+
+```
+> yarn watch-ts
+```
+
+## Deploy to Heroku
+
+Heroku will compile the Typescript into Javascript using the `postinstall` script.
+
+```
+> git push heroku main
+```
--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 import * as dotenv from "dotenv";
+import path from "path";
 dotenv.config({ path: __dirname+'/.env' });
 
 const app = express();
 const PORT = process.env.PORT;
+
+const usersRouter = require('./routes/users');
+//app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req,res) => {
     res.send('Express + TypeScript Server???')
 });
+
+app.use('/users', usersRouter);
+
+
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
