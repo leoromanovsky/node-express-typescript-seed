14408db8a9d7bb798312ea8ec7b9a9240cf59ac5
Leo Romanovsky <leoromanovsky@gmail.com>
2021-07-23 22:28:51 -0700

    read port from env

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
+
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.get('/', (req,res) => {
     res.send('Express + TypeScript Server???')
 });
