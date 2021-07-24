06d813f19304d46b336ce420cc18d04b6f37478a
Leo Romanovsky <leoromanovsky@gmail.com>
2021-07-23 22:34:52 -0700

    add dotenv package

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 app.get('/', (req,res) => {
     res.send('Express + TypeScript Server???')
 });
 app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running!! at https://localhost:${PORT}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
